f217d526c29bf377a2a7bb7803b3e3d1f8ce099d
ytung.dev@PC <ytung.dev@gmail.com>
2025-03-25 00:07:24 +0000

    finished Q3169

---
+++ 3169_count-days-without-meetings/solution.py
@@ -0,0 +1,20 @@
+# Beats 84.73%
+
+from typing import List
+
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings = sorted(meetings, key=lambda x:x[0])
+        last_meeting = [0,0]
+        ans = 0
+        for meeting in meetings:
+                a1,a2 = last_meeting
+                b1,b2 = meeting
+                # no overlap
+                if b1 - a2 > 1:
+                    ans+=b1-a2-1
+                    last_meeting = meeting
+                else:
+                    last_meeting = min(a1,b1), max(a2,b2)
+        ans += days - last_meeting[1]
+        return ans
