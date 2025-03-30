ea47c9986efb5ca4f33a5a019eafa907ac226c7d
Yi <ytung.dev@gmail.com>
2025-03-30 17:11:59 +0100

    added solution.py template creation

--- scrapper/main.py
+++ scrapper/main.py
@@ -65,6 +65,9 @@ def main():
         file.write('\n\n\n## Solution\n\n')
         file.write('- time  : O()\n')
         file.write('- space : O()\n')
+    with open(f"{new_dir}/solution.py", "w", encoding="utf-8") as file:
+        file.write('from typing import List\n\n')
+        file.write('# Time : Beats 0000 %\n# Memo : Beats 0000 %\n')
     print(f'create : {new_dir}')
     
     # write .csv log
