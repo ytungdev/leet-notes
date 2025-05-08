6c2668847842865caa8896954c12bf46c56e11a8
ytung.dev@PC <ytung.dev@gmail.com>
2025-05-09 00:45:58 +0100

    Added refactored solution

--- 3341_find-minimum-time-to-reach-last-room-i/solution.py
+++ 3341_find-minimum-time-to-reach-last-room-i/solution.py
@@ -1,9 +1,39 @@
 from typing import List
 import heapq
 
+# Time : Beats 97.21 %
+# Memo : Beats 90.35 %
+'''
+optimised:
+- Removed `dist`:
+    - Time of last node is passed by state in heap
+- Removed `if time < dist[u][v]`:
+    - Each node will only be visited once, so this condition is always true
+'''
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        m = len(moveTime)
+        n = len(moveTime[0])
+
+        inf = float('inf')
+        visited = [[False]*n for y in range(m)]
+        heap = []
+        
+        visited[0][0] = True
+        heapq.heappush(heap,(0,0,0))
+
+        while heap:
+            t,i,j = heapq.heappop(heap)
+            if i == m-1 and j == n-1:
+                return t
+            for u,v in [(i,j+1),(i,j-1),(i+1,j),(i-1,j)]:
+                if 0 <= u < m and 0<= v < n and not visited[u][v]:
+                    visited[u][v] = True
+                    heapq.heappush(heap, (max(t, moveTime[u][v]) + 1, u,v))
+
 # Time : Beats 90.17 %
 # Memo : Beats 98.20 %
-class Solution:
+class Solution1:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
         m = len(moveTime)
         n = len(moveTime[0])
--- 3342_find-minimum-time-to-reach-last-room-ii/solution.py
+++ 3342_find-minimum-time-to-reach-last-room-ii/solution.py
@@ -1,9 +1,41 @@
 from typing import List
 import heapq
 
-# Time : Beats 94.50 %
-# Memo : Beats 82,85 %
+# Time : Beats 96.76 %
+# Memo : Beats 94.76, %
+'''
+optimised:
+- Removed `dist`:
+    - Time of last node is passed by state in heap
+- Removed `if time < dist[u][v]`:
+    - Each node will only be visited once, so this condition is always true
+'''
 class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        m = len(moveTime)
+        n = len(moveTime[0])
+
+        inf = float('inf')
+        visited = [[False]*n for y in range(m)]
+        heap = []
+        
+        visited[0][0] = True
+        heapq.heappush(heap,(0,0,0,2))
+
+        while heap:
+            t,i,j,s = heapq.heappop(heap)
+            if i == m-1 and j == n-1:
+                return t
+            for u,v in [(i,j+1),(i,j-1),(i+1,j),(i-1,j)]:
+                if 0 <= u < m and 0<= v < n and not visited[u][v]:
+                    visited[u][v] = True
+                    step = 2 if s == 1 else 1
+                    time = max(t, moveTime[u][v]) + step
+                    heapq.heappush(heap, (time, u,v, step))
+
+# Time : Beats 94.50 %
+# Memo : Beats 82.85 %
+class Solution1:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
         m = len(moveTime)
         n = len(moveTime[0])
